e61c24f742d73bcf3cac1dc7532ccf5c1f4995db
Guilherme <deividasilva185@gmail.com>
2025-06-10 11:38:42 -0300

    a

--- resources/views/leads/index.blade.php
+++ resources/views/leads/index.blade.php
@@ -1,36 +1,40 @@
 @extends('layouts.admin')
-@php
-  
-@endphp
 @section('page-title')
-   
+    {{ __('Aplicativos') }}
+@endsection
+@section('breadcrumb')
+    <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">{{ __('Home') }}</a></li>
+    <li class="breadcrumb-item active" aria-current="page">{{ __('Aplicativos') }}</li>
 @endsection
 @section('title')
     <div class="d-inline-block">
-        @if (Auth::user()->type == 'super admin')
-            <h5 class="h4 d-inline-block font-weight-bold mb-0 text-white">{{ __('Settings') }}</h5>
-        @else
-            <h5 class="h4 d-inline-block font-weight-bold mb-0 text-white">{{ __('Store Settings') }}</h5>
-        @endif
+        <h5 class="h4 d-inline-block text-white font-weight-bold mb-0 ">{{ __('Aplicativos') }}</h5>
     </div>
 @endsection
 
-@endsection
-
-@push('script-page')
-    <script src="{{ asset('custom/libs/summernote/summernote-bs4.js') }}"></script>
+@push('css-page')
 
 @endpush
+
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <div class="card rounded">
-           
+    <div class="aplicativos-container">
+        <div class="container">
+            <!-- Header Section -->
+            <div class="text-center mb-5">
+                <h1 class="section-title">Aplicativos Premium</h1>
+                <p class="lead text-muted">Potencialize seu e-commerce com ferramentas profissionais</p>
             </div>
+
+            <!-- Search Bar -->
+            <div class="search-container mb-5">
+                <input type="text" class="search-input" placeholder="Pesquisar aplicativos..." id="searchApp">
+                <i class="fas fa-search search-icon"></i>
+            </div>
+
         </div>
     </div>
 @endsection
-@push('script-page')
-    <script src="{{ asset('custom/libs/jquery-mask-plugin/dist/jquery.mask.min.js') }}"></script>
 
-@endpush
+@push('script-page')
+
+@endpush 
